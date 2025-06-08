975c018a1bdd4f74b0f068ce60fe21b30b2ababd
topMira2223 <topmira2223@gmail.com>
2025-06-08 13:59:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/repo-name/' : '/',
   server: {
     host: "::",
     port: 8080,
